3a3dfc59ddd2e553d3e369b706e7ea8efbeef74d
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-10-13 07:42:59 +0200

    mulle/bootloader: Python3 compatibility in make_image.py

--- platform/mulle/bootloader/make_image.py
+++ platform/mulle/bootloader/make_image.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # Copyright (c) 2013, Eislab, Lulea University of Technology
 
+from __future__ import print_function
+
 import sys, time, os.path, operator, serial, struct, os, math
-__author__="Henrik Makitaavola"
+__author__ = "Henrik Makitaavola"
 
 # Some constants
 serial_speed = 38400
@@ -62,20 +64,20 @@ def buildImage(ihex):
                 offset = int(line[9:9+4], 16) << 4
             elif rectype == 0x01:
                 all.append((start_addr, section))
-                print start_addr
+                print(start_addr)
                 section = []
                 start_addr = addr
         except:
-            print "Unable to parse \"%s\", check that it really is a ihex file." % ihex
+            print("Unable to parse \"%s\", check that it really is a ihex file." % (ihex,))
             return []
-    
+
     #Pop all sections except the first
     for e in all:
-      print e[0]
+      print(e[0])
     for i in range(1, len(all)):
       all.pop()
     print('Ihex read complete:')
-    print('  ' + '\n  '.join(["%5d bytes starting at 0x%X" % (len(l), a) for (a, l) in all]))
+    print(('  ' + '\n  '.join(["%5d bytes starting at 0x%X" % (len(l), a) for (a, l) in all])))
     #print('  %d bytes in %d sections\n' % (reduce(operator.add, [len(l) for (_, l) in all]), len(all)))
 
     #all_data = []
@@ -107,25 +109,25 @@ def calc_crc32(data):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print "Usage: %s <ihex> <output>" % sys.argv[0]
-        print "  <ihex> is the application program in ihex format."
-        print "  <output> is the name of the file to store the uploadable ihex data in."
-        print "  <start address> expected start address of application in memory"
+        print("Usage: %s <ihex> <output>" % sys.argv[0])
+        print("  <ihex> is the application program in ihex format.")
+        print("  <output> is the name of the file to store the uploadable ihex data in.")
+        print("  <start address> expected start address of application in memory")
     else:
         try:
             os.stat(sys.argv[1])         # Checks whether the specified file is valid
         except:
-            print "ERROR: Unable to open file \"%s\"." % sys.argv[1]
+            print("ERROR: Unable to open file \"%s\"." % sys.argv[1])
             exit(1)
         #  Create the binary image
         data = buildImage(sys.argv[1])
         if len(data) != 1:
-            print "ERROR: Only able to handle one section"
+            print("ERROR: Only able to handle one section")
             exit(1)
         #print data[0][0]
         start = int(sys.argv[3], 16)
         if (data[0][0] != start):
-            print "ERROR: Expected start address 0x%X but got 0x%X" %(start, data[0][0])
+            print("ERROR: Expected start address 0x%X but got 0x%X" %(start, data[0][0]))
             exit(1)
         f = open(sys.argv[2], 'w')
         cnr = 0
@@ -140,7 +142,7 @@ if __name__ == "__main__":
         f.write(chr(crc32[1]))
         f.write(chr(crc32[2]))
         f.write(chr(crc32[3]))
-        
+
         for d in data[0][1]:
             f.write(chr(d))
             crc = calc_crc(crc, d)
